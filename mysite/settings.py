2dfeece81745c0b9f23c823e9c71b6a5bfbe6d13
Carlos Ribeiro <carlos.rib.rocha@gmail.com>
2023-06-28 09:17:15 -0300

    commit settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6new%xxj^am(=yn6j2l*%47pj%riol!#wwj1h@urc@j9gck(j$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
